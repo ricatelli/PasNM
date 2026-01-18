3605dd3b23b235dc20776499466867e66490a63e
ricatelli <ricardoromitelli@gmail.com>
2026-01-17 22:42:04 -0300

    Update ws.js

--- ui/ws.js
+++ ui/ws.js
@@ -1,8 +1,11 @@
-let ws;
-
 export function wsConnect() {
+  if (location.protocol !== "http:") {
+    console.warn("WS bloqueado: origen HTTPS");
+    return;
+  }
+
   const host = location.hostname;
-  ws = new WebSocket(`ws://${host}:81/`);
+  const ws = new WebSocket(`ws://${host}:81/`);
 
   ws.onopen = () => console.log("WS conectado");
   ws.onmessage = e => console.log("WS RX:", e.data);
