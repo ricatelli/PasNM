6be496ac40f968da6b3df8b7cfa8d5bd6defdf4b
ricatelli <ricardoromitelli@gmail.com>
2026-01-17 22:23:03 -0300

    Update ws.js

--- ui/ws.js
+++ ui/ws.js
@@ -1,10 +1,8 @@
-const ESP_IP = "192.168.5.172";
-const WS_PORT = 81;
-
 let ws;
 
 export function wsConnect() {
-  ws = new WebSocket(`ws://${ESP_IP}:${WS_PORT}/`);
+  const host = location.hostname;
+  ws = new WebSocket(`ws://${host}:81/`);
 
   ws.onopen = () => console.log("WS conectado");
   ws.onmessage = e => console.log("WS RX:", e.data);
