013f72c3f18626b26b52c61b14de039ff87b645f
ricatelli <ricardoromitelli@gmail.com>
2026-01-17 23:41:43 -0300

    Update app.js

--- ui/app.js
+++ ui/app.js
@@ -1,3 +1,8 @@
 import { wsConnect } from "./ws.js";
 
-wsConnect();
+// SOLO conectar WS si la UI corre desde HTTP (ESP)
+if (location.protocol === "http:") {
+  wsConnect();
+} else {
+  console.log("UI en HTTPS (instalación / actualización)");
+}
