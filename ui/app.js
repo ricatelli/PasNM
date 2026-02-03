b1182fa2ffcc1f1af733a5dfd642748b385e764d
ricatelli <ricardoromitelli@gmail.com>
2026-02-03 02:07:26 -0300

    Delete ui/app.js

--- ui/app.js
+++
@@ -1,8 +0,0 @@
-import { wsConnect } from "./ws.js";
-
-// SOLO conectar WS si la UI corre desde HTTP (ESP)
-if (location.protocol === "http:") {
-  wsConnect();
-} else {
-  console.log("UI en HTTPS (instalación / actualización)");
-}
